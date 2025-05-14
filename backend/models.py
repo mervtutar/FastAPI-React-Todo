764d6c17eaad96da6f708aa53b7591422c75537e
Merve Tutar <mervetutar78@gmail.com>
2025-05-14 13:40:35 +0300

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Todo(Base):
+    __tablename__ = "todo"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+
+
+
+
+
+
+
