483fc5ade35b80e1ece98c28fc44b5a4fc496d0d
Merve Tutar <mervetutar78@gmail.com>
2025-05-18 02:21:17 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from database import SessionLocal, engine
 from models import Todo, Base
@@ -7,15 +7,18 @@ from pydantic import BaseModel
 Base.metadata.create_all(bind=engine) # models dosyasındaki tüm tabloları veritabanında oluşturur
 
 app = FastAPI()
+
+# API ye kimlerin istek atabileceğini belirtiyoruz
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['*'], # herkesi kabul et
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-def get_db(): # veri tabanına bağlanmak için
+# veri tabanına bağlanmak için
+def get_db(): 
     db = SessionLocal()
     try:
         yield db
@@ -30,7 +33,7 @@ def root():
 def say_hi():
     return {"message": "Hi"}'''
 
-
+# getirmek için
 @app.get("/todos") # bütün todo ları listeleyeceğimiz fonksiyon
 def read_todos(db = Depends(get_db)):
     todos = db.query(Todo).all() # database den tüm verileri getir
@@ -39,11 +42,50 @@ def read_todos(db = Depends(get_db)):
 class TodoCreate(BaseModel):
     title: str
 
-@app.post("/todos")
+@app.post("/todos") # oluşturmak için
 def create_todo(new_todo: TodoCreate, db = Depends(get_db)):
     db_todo = Todo(title=new_todo.title)
     db.add(db_todo)
     db.commit()
     return {"message": "Successfully Added"}
 
+class TodoUpdate(BaseModel):
+    title : str
 
+# update için
+@app.put("/todos/{todo_id}")
+def update_todo(
+    todo_id: int,
+    request_data: TodoUpdate,
+    db = Depends(get_db)
+
+):
+    todo_item = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if not todo_item:
+        raise HTTPException(status_code=404, detail="todo bulunamadi")
+
+    todo_item.title = request_data.title
+
+    db.add(todo_item)
+
+    db.add(todo_item)
+    db.commit()
+
+    return {"message": "Successfully updated"}
+
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(
+    todo_id: int,
+    db = Depends(get_db)
+):
+    todo_item = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if not todo_item:
+        raise HTTPException(status_code=404, detail="todo bulunamadi")
+
+    db.delete(todo_item)
+    db.commit()
+
+    return {"message": "Successfully deleted"}
